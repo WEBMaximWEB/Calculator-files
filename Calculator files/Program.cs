e48d2f0eb5faca8ed51f0137485e96fddeacc44e
p-MAX-q <pmaxq@users.noreply.github.com>
2020-10-30 17:51:08 +0500

    Update Program.cs

--- Calculator files/Program.cs
+++ Calculator files/Program.cs
@@ -43,66 +43,140 @@ namespace Calculator_files
                         j++;
                     }
                     line = line.Remove(j - text.Length, text.Length + 1);
-                    line = line.Insert(i, Brackets(text));
+                    line = line.Insert(i, Check(text));
                 }
             }
+            line = Check(line);
             Console.WriteLine("----->" + line);
         }
 
-        static string Brackets(string text)
+        static string Brackets(string text, char[] xDictionary)
         {
-            char[] bigDictionary = { '*', '/' };
-            string str = "", digit1 = "", digit2 = "";
+            string digit1 = "", digit2 = "";
+            int count = 0;
             for (int i = 0; i < text.Length; i++)
             {
-                if (bigDictionary.Contains(text[i]))
+                if (xDictionary.Contains(text[i]))
                 {
                     int j = i + 1;
                     while (j < text.Length && Char.IsDigit(text[j]))
                     {
                         digit1 += text[j];
+                        Console.WriteLine("d1=" + digit1);
                         j++;
                     }
-                    if (i > 0)
+                    if (i >= 0)
                     {
                         j = i - 1;
                         while (j >= 0 && Char.IsDigit(text[j]))
                         {
+                            //if (text[j] == '+' || text[j] == '-')
+                              //  break;
                             digit2 += text[j];
+                            Console.WriteLine("d2=" + digit2);
                             j--;
                         }
-                        string s = "";
-                        for (int k = digit2.Length - 1; k >= 0; k--)
-                            s += digit2[k];
-                        digit2 = s;
                     }
+                    count = i;
+                    break;
                 }
-                if (text[i] == '*')
-                {
-                    int num1 = Int32.Parse(digit1);
-                    int num2 = Int32.Parse(digit2);
-
-                    text = text.Remove(i, 1);
-                    str = (num1 * num2).ToString();
-                    Console.WriteLine(digit2);
-                    Console.WriteLine("->" + text);
-                }
-                else if (text[i] == '/')
-                {
-                    int num1 = Int32.Parse(digit1), num2 = Int32.Parse(digit2);
-                    
-                    text = text.Remove(i, 1);
-                    if (num2 == 0)
-                    {
-                        Console.WriteLine("Нельзя делить на ноль!");
-                        return "0";
-                    }
-                    str = (num1 / num2).ToString();
-                }
-                text = text.Remove(i - digit2.Length, digit1.Length + digit2.Length + 1);
             }
-            Console.WriteLine("===" + str);
-            return str + "(" +text + ")";
+            string s = "";
+            for (int k = digit2.Length - 1; k >= 0; k--)
+                s += digit2[k];
+            digit2 = s;
+            return MainComputation(text, digit1, digit2, count);
+        }
+
+        static string MainComputation(string text, string digit1, string digit2, int i)
+        {
+            Console.WriteLine("попал в main");
+            string str = "";
+            if (text[i] == '*')
+            {
+                int num1 = Int32.Parse(digit1);
+                int num2 = Int32.Parse(digit2);
+
+                str = (num1 * num2).ToString();
+            }
+            else if (text[i] == '/')
+            {
+                int num1 = Int32.Parse(digit1), num2 = Int32.Parse(digit2);
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Нельзя делить на ноль!");
+                    return "0";
+                }
+                str = (num2 / num1).ToString();
+            }
+            else if (text[i] == '+')
+            {
+                Console.WriteLine("Проверка на плюс");
+                int num1 = Int32.Parse(digit1);
+                int num2 = Int32.Parse(digit2);
+
+                str = (num2 + num1).ToString();
+            }
+            else if (text[i] == '-')
+            {
+                Console.WriteLine("Проверка на минус");
+                Console.WriteLine("Это digit1:" + digit1);
+                int num1 = Int32.Parse(digit1);
+                int num2 = Int32.Parse(digit2);
+
+                str = (num2 - num1).ToString();
+            }
+            Console.WriteLine(text);
+            int len = digit1.Length + digit2.Length + 1;
+            int position = i - digit2.Length;
+            Console.WriteLine("i=" + i + ", digit2L= " + digit2.Length + "textL=" + text.Length +
+                "len = " + len +  " position = " + position + "digit2 =" + digit2 + 
+                " digit1 = " + digit1 + "текст до удаления:" + text);
+            text = text.Remove(position, len);
+            Console.WriteLine("len ===" + len);
+            Console.WriteLine("str + text= " + str + text + "на самом деле" + text.Insert(i - digit2.Length, str));
+            return text.Insert(i - digit2.Length, str);
+        }
+
+        static string Check(string str)
+        {
+            char[] bigDictionary = { '*', '/' };
+            char[] smallDictionary = { '+', '-' };
+            bool flag = true;
+            
+            // поиск умножения и деления
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (bigDictionary.Contains(str[i]))
+                    {
+                        str = Brackets(str, bigDictionary);
+                        i = str.Length + 1;
+                        flag = true;
+                        Console.WriteLine("*********--->" + str);
+                    }
+                }
+            }
+            // поиск сложения и вычитания
+            flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (smallDictionary.Contains(str[i]))
+                    {
+                        Console.WriteLine("до смола--->" + str);
+                        str = Brackets(str, smallDictionary);
+                        i = str.Length + 1;
+                        flag = true;
+                        Console.WriteLine("SmallDict*****->>>" + str);
+                    }
+                }
+            }
+            return str;
         }
     }
 }
