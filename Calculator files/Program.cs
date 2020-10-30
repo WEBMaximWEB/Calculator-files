4a05d855ae2eaa7018e9db59126cdf9aeeca21b2
p-MAX-q <pmaxq@users.noreply.github.com>
2020-10-30 18:18:55 +0500

    Update Program.cs

--- Calculator files/Program.cs
+++ Calculator files/Program.cs
@@ -12,6 +12,8 @@ namespace Calculator_files
             string line = "";
             char[] dictionary = {'+', '-', '*', '/', '(', ')'};
             int counter = 0;
+
+            // Пример:  1 +( 34 * 4 - 8/ 4+  3  )* 2   4 
             StreamReader f = new StreamReader(
                                        "C:/Users/pmaxq/OneDrive/Desktop/input.txt");
             while (line == "" && counter < 20)
@@ -19,6 +21,9 @@ namespace Calculator_files
                 line += f.ReadLine();
                 counter++;
             }
+            f.Close();
+
+            //далее работа со стракой
             line = line.Replace(" ", "");
             Console.WriteLine(line);
 
@@ -31,6 +36,7 @@ namespace Calculator_files
                 }
             }
 
+            //работа со скобками
             string text = "";
             for (int i = line.Length - 1; i >= 0; i--)
             {
@@ -49,7 +55,6 @@ namespace Calculator_files
             }
             line = Check(line);
             Console.WriteLine("Готово.");
-
 
             StreamWriter sw = new StreamWriter("C:/Users/pmaxq/OneDrive/Desktop/output.txt");
             sw.WriteLine(line);
@@ -88,22 +93,18 @@ namespace Calculator_files
             for (int k = digit2.Length - 1; k >= 0; k--)
                 s += digit2[k];
             digit2 = s;
-            return MainComputation(text, digit1, digit2, count);
+            return MainComputation(text, Int32.Parse(digit1), Int32.Parse(digit2), count);
         }
 
-        static string MainComputation(string text, string digit1, string digit2, int i)
+        static string MainComputation(string text, int num1, int num2, int i)
         {
             string str = "";
             if (text[i] == '*')
             {
-                int num1 = Int32.Parse(digit1);
-                int num2 = Int32.Parse(digit2);
-
                 str = (num1 * num2).ToString();
             }
             else if (text[i] == '/')
             {
-                int num1 = Int32.Parse(digit1), num2 = Int32.Parse(digit2);
                 if (num2 == 0)
                 {
                     Console.WriteLine("Нельзя делить на ноль!");
@@ -113,22 +114,22 @@ namespace Calculator_files
             }
             else if (text[i] == '+')
             {
-                int num1 = Int32.Parse(digit1);
-                int num2 = Int32.Parse(digit2);
-
                 str = (num2 + num1).ToString();
             }
             else if (text[i] == '-')
             {
-                int num1 = Int32.Parse(digit1);
-                int num2 = Int32.Parse(digit2);
+                if (num2 != 0)
+                {
+                    str = (num2 - num1).ToString();
+                }
+                else
+                    str = "-" + num1.ToString();
 
-                str = (num2 - num1).ToString();
             }
-            int len = digit1.Length + digit2.Length + 1;
-            int position = i - digit2.Length;
+            int len = num1.ToString().Length + num2.ToString().Length + 1;
+            int position = i - num2.ToString().Length;
             text = text.Remove(position, len);
-            return text.Insert(i - digit2.Length, str);
+            return text.Insert(i - num2.ToString().Length, str);
         }
 
         static string Check(string str)
